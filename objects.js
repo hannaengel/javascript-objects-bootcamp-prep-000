6c031bff86478370b933aaf29912b0059cc25c3f
Hanna Engel <hengel@alumni.berklee.edu>
2019-03-30 21:01:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
 var playlist = {My Bloody Valentine:'Sometimes', Slowdive:'Alison'};
 
 function updatePlaylist(playlist, artist, song){
-  playlist.artist = song
-  return playlist
+  playlist.artist = song;
+  return playlist;
 }
 
 function removeFromPlaylist (playlist, artist){
-  delete playlist.artist
-  return playlist
+  delete playlist.artist;
+  return playlist;
 }
